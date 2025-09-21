571046dfa090595439769ef19b551991a253f45f
CamiloRaG5 <camilo.ramirezg@udea.edu.co>
2025-09-20 20:10:52 -0500

    Integración con API de clima (Open-Meteo)

--- SistemaRecomendacion.pl
+++ SistemaRecomendacion.pl
@@ -11,3 +11,18 @@ coords(pereira,    4.8133, -75.6961).
 coords(bogota,     4.6097, -74.0817).
 coords(cali,       3.4372, -76.5225).
 coords(cartagena, 10.3997, -75.5144).
+
+% Obtener clima desde Open-Meteo
+% get_weather(City, Weather).
+% Weather -> etiqueta simplificada (soleado, nublado, lluvioso, nevado, tormenta)
+
+get_weather(City, Weather) :-
+    coords(City, Lat, Lon),
+    format(string(URL),
+           "https://api.open-meteo.com/v1/forecast?latitude=~w&longitude=~w&current_weather=true",
+           [Lat, Lon]),
+    http_open(URL, Stream, []),
+    json_read_dict(Stream, Dict),
+    close(Stream),
+    Code = Dict.current_weather.weathercode,
+    traducir_codigo(Code, Weather).
